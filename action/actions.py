ee153d70f20893025d340efaf1915c18639299e3
Kevin Wang <wy721@qq.com>
2019-03-11 18:36:27 +0800

    添加酒店formaction示例

--- action/actions.py
+++ action/actions.py
@@ -1,81 +1,59 @@
 # coding: utf8
 from rasa_core_sdk import Action
-from rasa_core_sdk.events import BotUttered
-import random
-import json
+from rasa_core_sdk.events import AllSlotsReset
+from rasa_core_sdk.forms import FormAction
+import typing
+
+if typing.TYPE_CHECKING:
+    from typing import Text
+    from rasa_core_sdk.executor import CollectingDispatcher, Tracker
 
 
-class ActionLookupInfo(Action):
-    contacts = json.load(open("contacts.json"))
-
+class BookRoomAskConfirm(Action):
     def name(self):
         # type: () -> Text
-        return "lookup_info"
+        return "ask_confirm"
 
     def run(self, dispatcher, tracker, domain):
         # type: (CollectingDispatcher, Tracker, Dict[Text, Any]) -> List[Dict[Text, Any]]
 
-        def format(info, item):
-            value = info['name'] + '\n'
-            if item in ("电话", "手机", "号码"):
-                value += " 手机: %s" % info["cellphone"]
-                if info["ext"].strip():
-                    value += " 分机: %s" % info["ext"]
-            elif item == "邮箱":
-                value += " 邮箱: %s" % info["email"]
-            else:
-                value += " 手机: %s" % info["cellphone"]
-                if info["ext"].strip():
-                    value += " 分机: %s" % info["ext"]
-                value += " 邮箱: %s" % info["email"]
-            return value
-
-        name = tracker.get_slot('lookup_name')
-        item = tracker.get_slot('lookup_item')
-        info = self.contacts.get(name)
-        if not info:
-            dispatcher.utter_message("没有找到{}的信息".format(name))
-            return []
-        if len(info) > 1:
-            dispatcher.utter_message("公司一共有{}个{}".format(len(info), name))
-        msg = ""
-        for i in info:
-            msg += format(i, item)
-        dispatcher.utter_message(msg)
         return []
 
 
-class ActionPlayMusic(Action):
+class BookRoom(FormAction):
     def name(self):
-        # type: () -> Text
-        return "play_music"
+        return "book_room"
 
-    def run(self, dispatcher, tracker, domain):
-        # type: (CollectingDispatcher, Tracker, Dict[Text, Any]) -> List[Dict[Text, Any]]
-        def random_music(artist):
-            if artist:
-                mlst = {
-                    "周杰伦": ["双截棍", "发如雪", "烟花易冷"],
-                    "陈奕迅": ["十年", "浮夸"],
-                }.get(artist)
-                return random.choice(mlst) if mlst else None
-            else:
-                return random.choice(["卡路里", "小苹果"])
+    @staticmethod
+    def required_slots(tracker):
+        return ['room_type', 'guest_name', 'guest_phone_number', 'checkin_time', 'confirmed']
 
-        name = tracker.get_slot('music_name')
-        artist = tracker.get_slot('music_artist')
-        if not name:
-            if artist:
-                name = random_music(artist)
-                if not name:
-                    dispatcher.utter_message("对不起，没有找到{}的歌曲".format(artist))
-                    artist = "周杰伦"
-                    name = random_music(artist)
-            else:
-                name = random_music(None)
-        if artist:
-            msg = "正在为您播放{}的《{}》".format(artist, name)
-        else:
-            msg = "正在为您播放《{}》".format(name)
-        dispatcher.utter_message(msg)
-        return []
+    def submit(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("完成")
+        return [AllSlotsReset()]
+
+    def slot_mappings(self):
+        return {'confirmed': [
+            self.from_intent(True, intent='ok'),
+            self.from_intent(True, intent='confirm')
+        ]}
+
+    def request_next_slot(self,
+                          dispatcher,  # type: CollectingDispatcher
+                          tracker,  # type: Tracker
+                          domain  # type: Dict[Text, Any]
+                          ):
+        events = super(BookRoom, self).request_next_slot(dispatcher, tracker, domain)
+        if events is not None and len(events) > 0:
+            evt = events[0]
+            if evt['event'] == 'slot' and evt['name'] == 'requested_slot' and evt['value'] == 'confirmed':
+                dispatcher.messages.pop()
+                dispatcher.utter_message("确认一下您的信息, 姓名:{} 电话：{} 预订一间{}, {}入住".format(
+                    tracker.get_slot("guest_name"),
+                    tracker.get_slot("guest_phone_number"),
+                    tracker.get_slot("room_type"),
+                    tracker.get_slot("checkin_time"),
+                ))
+        return events
+
+
--- business/stories.md
+++ business/stories.md
@@ -6,19 +6,36 @@
 * bye
   - utter_bye
 
-## play music
-* request_music
-  - play_music
+## book room 0
+* book_room{"room_type": "r"}
+  - book_room
+  - form{"name": "book_room"}
+  - form{"name": null}
 
-## call
-* call
-  - utter_call
-
-## give ip
-* request_ip
-  - utter_give_ip
-
-## info
-* lookup_info
-  - lookup_info
+## book room 1
+* book_room{"room_type": "r"}
+  - book_room
+  - slot{"room_type": "r"}
+  - slot{"requested_slot": "guest_name"}
+  - utter_ask_guest_name
+* info{"guest_name": "r"}
+  - book_room
+  - slot{"guest_name": "r"}
+  - slot{"requested_slot": "guest_phone_number"}
+  - utter_ask_guest_phone_number
+* info{"guest_phone_number": "18088880000"}
+  - book_room
+  - slot{"guest_phone_number": "18088880000"}
+  - slot{"requested_slot": "checkin_time"}
+  - utter_ask_checkin_time
+* info{"checkin_time": "today"}
+  - book_room
+  - slot{"checkin_time": "today"}
+  - slot{"requested_slot": "confirmed"}
+  - utter_ok
+  - ask_confirm
+* confirm
+  - book_room
+  - slot{"confirmed": true}
+  - utter_bye
 
