ca9193000802f31a5a65a2c248caaf6dc3ef638c
Kevin Wang <wy721@qq.com>
2019-04-23 15:39:00 +0800

    Remove tf.flags

--- nlu/bert_ner/ner.py
+++ nlu/bert_ner/ner.py
@@ -16,87 +16,33 @@ from .bert import optimization
 import tensorflow as tf
 import os
 
-BERT_MODEL_FILE_NAME = "bert_model.pkl"
-
-flags = tf.flags
-
-FLAGS = flags.FLAGS
-
-flags.DEFINE_string(
-    "bert_config_file", "bert_ner/checkpoint/bert_config.json",
-    "The config json file corresponding to the pre-trained BERT model."
-)
-
-flags.DEFINE_string(
-    "task_name", "NER", "The name of the task to train."
-)
-
-flags.DEFINE_string(
-    "output_dir", "bert_ner/output/result_dir",
-    "The output directory where the model checkpoints will be written."
-)
-
-## Other parameters
-flags.DEFINE_string(
-    "init_checkpoint", "bert_ner/checkpoint/bert_model.ckpt",
-    "Initial checkpoint (usually from a pre-trained BERT model)."
-)
-
-flags.DEFINE_bool(
-    "do_lower_case", True,
-    "Whether to lower case the input text."
-)
-
-flags.DEFINE_integer(
-    "max_seq_length", 128,
-    "The maximum total input sequence length after WordPiece tokenization."
-)
-
-flags.DEFINE_bool(
-    "do_train", True,
-    "Whether to run training."
-)
-flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
-
-flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
-
-flags.DEFINE_bool("do_predict", False,"Whether to run the model in inference mode on the test set.")
-
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
-
-flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
-
-flags.DEFINE_integer("predict_batch_size", 8, "Total batch size for predict.")
-
-flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
-
-flags.DEFINE_float("num_train_epochs", 10.0, "Total number of training epochs to perform.")
-
-
-
-flags.DEFINE_float(
-    "warmup_proportion", 0.1,
-    "Proportion of training to perform linear learning rate warmup for. "
-    "E.g., 0.1 = 10% of training.")
-
-flags.DEFINE_integer("save_checkpoints_steps", 1000,
-                     "How often to save the model checkpoint.")
-
-flags.DEFINE_integer("iterations_per_loop", 1000,
-                     "How many steps to make in each estimator call.")
-
-flags.DEFINE_string("vocab_file", None,
-                    "The vocabulary file that the BERT model was trained on.")
-tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
-flags.DEFINE_integer(
-    "num_tpu_cores", 8,
-    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
 
 class BertExtractor(EntityExtractor):
+    # ===== rasa configs ======
     name = "ner_bert"
     provides = ["entities"]
+
+    # ===== bert configs ======
+    BERT_MODEL_FILE_NAME = "bert_model.pkl"
     input_length = 128
     max_seq_length = 128
+    use_tpu = False
+    tpu_name = ""
+    config_file = "bert_ner/checkpoint/bert_config.json"
+    tpu_zone = None
+    gcp_project = None
+    master = None  # [Optional] TensorFlow master URL.
+    output_dir = "bert_ner/output/result_dir"
+    save_checkpoints_steps = 1000  # How often to save the model checkpoint.
+    iterations_per_loop = 1000  # How many steps to make in each estimator call.
+    num_tpu_cores = 8  # Only used if `use_tpu` is True. Total number of TPU cores to use.
+    train_batch_size = 32
+    num_train_epochs = 10
+    warmup_proportion = 0.1  # Proportion of training to perform linear learning rate warmup for.
+    init_checkpoint = "bert_ner/checkpoint/bert_model.ckpt"
+    learning_rate = 5e-5
+    eval_batch_size = 8
+    predict_batch_size = 8
 
     def train(self, training_data, config, **kwargs):
         # type: (TrainingData, RasaNLUModelConfig, **Any) -> None
@@ -104,72 +50,70 @@ class BertExtractor(EntityExtractor):
         self.dataset = create_dataset(training_data.training_examples)
         train_file = "train.tf_record"  # todo
         self._prepare_features(train_file)
-        if not FLAGS.do_train:
-            raise ValueError("At least one of 'do_train' of 'do_eval' must be True.")
-        bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
+        bert_config = modeling.BertConfig.from_json_file(self.config_file)
 
-        if FLAGS.max_seq_length > bert_config.max_position_embeddings:
+        if self.max_seq_length > bert_config.max_position_embeddings:
             raise ValueError(
                 "Cannot use sequence length %d because the BERT model "
                 "was only trained up to sequence length %d" %
-                (FLAGS.max_seq_length, bert_config.max_position_embeddings)
+                (self.max_seq_length, bert_config.max_position_embeddings)
             )
 
         tpu_cluster_resolver = None
-        if FLAGS.use_tpu and FLAGS.tpu_name:
+        if self.use_tpu and self.tpu_name:
             tpu_cluster_resolver = tf.contrib.cluster_resolver.TPUClusterResolver(
-                FLAGS.tpu_name, zone=FLAGS.tpu_zone, project=FLAGS.gcp_project)
+                self.tpu_name, zone=self.tpu_zone, project=self.gcp_project)
 
         is_per_host = tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2
 
         run_config = tf.contrib.tpu.RunConfig(
             cluster=tpu_cluster_resolver,
-            master=FLAGS.master,
-            model_dir=FLAGS.output_dir,
-            save_checkpoints_steps=FLAGS.save_checkpoints_steps,
+            master=self.master,
+            model_dir=self.output_dir,
+            save_checkpoints_steps=self.save_checkpoints_steps,
             tpu_config=tf.contrib.tpu.TPUConfig(
-                iterations_per_loop=FLAGS.iterations_per_loop,
-                num_shards=FLAGS.num_tpu_cores,
+                iterations_per_loop=self.iterations_per_loop,
+                num_shards=self.num_tpu_cores,
                 per_host_input_for_training=is_per_host))
 
         train_examples = None
         num_train_steps = None
         num_warmup_steps = None
 
-        if FLAGS.do_train:
+        if True:  # FLAGS.do_train:
             train_examples = self.dataset.examples
             num_train_steps = int(
-                len(train_examples) / FLAGS.train_batch_size * FLAGS.num_train_epochs)
-            num_warmup_steps = int(num_train_steps * FLAGS.warmup_proportion)
+                len(train_examples) / self.train_batch_size * self.num_train_epochs)
+            num_warmup_steps = int(num_train_steps * self.warmup_proportion)
 
         model_fn = self._model_fn_builder(
             bert_config=bert_config,
             num_labels=len(self.dataset.labels) + 1,
-            init_checkpoint=FLAGS.init_checkpoint,
-            learning_rate=FLAGS.learning_rate,
+            init_checkpoint=self.init_checkpoint,
+            learning_rate=self.learning_rate,
             num_train_steps=num_train_steps,
             num_warmup_steps=num_warmup_steps,
-            use_tpu=FLAGS.use_tpu,
-            use_one_hot_embeddings=FLAGS.use_tpu)
+            use_tpu=self.use_tpu,
+            use_one_hot_embeddings=self.use_tpu)
 
         estimator = tf.contrib.tpu.TPUEstimator(
-            use_tpu=FLAGS.use_tpu,
+            use_tpu=self.use_tpu,
             model_fn=model_fn,
             config=run_config,
-            train_batch_size=FLAGS.train_batch_size,
-            eval_batch_size=FLAGS.eval_batch_size,
-            predict_batch_size=FLAGS.predict_batch_size)
+            train_batch_size=self.train_batch_size,
+            eval_batch_size=self.eval_batch_size,
+            predict_batch_size=self.predict_batch_size)
 
         self.ent_tagger = estimator
 
-        if FLAGS.do_train:
+        if True:  # FLAGS.do_train:
             tf.logging.info("***** Running training *****")
             tf.logging.info("  Num examples = %d", len(train_examples))
-            tf.logging.info("  Batch size = %d", FLAGS.train_batch_size)
+            tf.logging.info("  Batch size = %d", self.train_batch_size)
             tf.logging.info("  Num steps = %d", num_train_steps)
             train_input_fn = self._file_based_input_fn_builder(
                 input_file=train_file,
-                seq_length=FLAGS.max_seq_length,
+                seq_length=self.max_seq_length,
                 is_training=True,
                 drop_remainder=True)
             self.ent_tagger.train(input_fn=train_input_fn, max_steps=num_train_steps)
@@ -303,17 +247,15 @@ class BertExtractor(EntityExtractor):
                     scaffold_fn = tpu_scaffold
                 else:
                     tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
-            tf.logging.info("**** Trainable Variables ****")
+                tf.logging.info("**** Trainable Variables ****")
 
-            for var in tvars:
-                init_string = ""
-                if var.name in initialized_variable_names:
-                    init_string = ", *INIT_FROM_CKPT*"
-                tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
-                                init_string)
+                for var in tvars:
+                    init_string = ""
+                    if var.name in initialized_variable_names:
+                        init_string = ", *INIT_FROM_CKPT*"
+                    tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
+                                    init_string)
 
-            output_spec = None
-            tf.trainable_variables = lambda: tune_vars
             if mode == tf.estimator.ModeKeys.TRAIN:
                 train_op = optimization.create_optimizer(
                     total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
@@ -349,11 +291,11 @@ class BertExtractor(EntityExtractor):
         from sklearn.externals import joblib
 
         if self.ent_tagger:
-            model_file_name = os.path.join(model_dir, BERT_MODEL_FILE_NAME)
+            model_file_name = os.path.join(model_dir, self.BERT_MODEL_FILE_NAME)
 
             joblib.dump(self.ent_tagger, model_file_name)
 
-        return {"classifier_file": BERT_MODEL_FILE_NAME}
+        return {"classifier_file": self.BERT_MODEL_FILE_NAME}
 
     def load(cls,
              model_dir=None,  # type: Optional[Text]
@@ -365,7 +307,7 @@ class BertExtractor(EntityExtractor):
         from sklearn.externals import joblib
 
         meta = model_metadata.for_component(cls.name)
-        file_name = meta.get("classifier_file", BERT_MODEL_FILE_NAME)
+        file_name = meta.get("classifier_file", cls.BERT_MODEL_FILE_NAME)
         model_file = os.path.join(model_dir, file_name)
 
         if os.path.exists(model_file):
@@ -383,16 +325,16 @@ class BertExtractor(EntityExtractor):
         predict_file = "predict.tf_record"  # todo
         self._prepare_features(predict_file)
         if self.ent_tagger is not None:
-            if FLAGS.use_tpu:
+            if self.use_tpu:
                 # Warning: According to tpu_estimator.py Prediction on TPU is an
                 # experimental feature and hence not supported here
                 raise ValueError("Prediction in TPU not supported")
-            predict_drop_remainder = True if FLAGS.use_tpu else False
+            predict_drop_remainder = True if self.use_tpu else False
 
 
             predict_input_fn = self._file_based_input_fn_builder(
                 input_file=predict_file,
-                seq_length=FLAGS.max_seq_length,
+                seq_length=self.max_seq_length,
                 is_training=False,
                 drop_remainder=predict_drop_remainder)
 
