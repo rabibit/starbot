72e0312e179d9fa5fe47444df670f5c76c87cabd
Kevin Wang <wy721@qq.com>
2019-05-04 22:28:06 +0800

    支持mitie pipelines’

--- run/nlu_train.py
+++ run/nlu_train.py
@@ -8,9 +8,15 @@ from starbot.nlu.train import train
 from starbot.utils.download import download
 from tempfile import TemporaryDirectory
 
-BERT_MODEL_URL = "http://cloud.kvin.wang:88/s/ZabQxpnJeHBymg6/download"
+BERT_MODEL_URL = "https://cloud.kvin.wang:8443/s/ZabQxpnJeHBymg6/download"
 BERT_MODEL_FILE = "checkpoint.zip"
 BERT_MODEL_DIRNAME = "chinese_L-12_H-768_A-12"
+
+MITIE_MODEL_URL = "https://cloud.kvin.wang:8443/s/XEeQkZeqYb7fDYT/download"
+MITIE_MODEL_FILE = "total_word_feature_extractor.dat"
+
+#CONFIG = "nlu_config.yml"
+CONFIG = "rasa_nlu_config.yml"
 
 logger = logging.getLogger(__name__)
 base = Path(__file__).parent
@@ -27,10 +33,23 @@ def download_bert_model_if_need():
             shutil.move(base / BERT_MODEL_DIRNAME, base / "checkpoint")
 
 
+def download_mitie_model_if_need():
+    if not base.joinpath(MITIE_MODEL_FILE).exists():
+        with TemporaryDirectory() as tmpdir:
+            tmpfilename = os.path.join(tmpdir, MITIE_MODEL_FILE)
+            logger.info('Downloading {}'.format(MITIE_MODEL_URL))
+            download(MITIE_MODEL_URL, tmpfilename)
+            logger.info('Download {} finished'.format(tmpfilename))
+            shutil.move(tmpfilename, base / MITIE_MODEL_FILE)
+
 def main():
     mdfile = base / 'data' / 'nlu.md'
-    nlu_config = base / 'nlu_config.yml'
-    download_bert_model_if_need()
+    nlu_config = base / CONFIG
+    if CONFIG == 'nlu_config.yml':
+        download_bert_model_if_need()
+    elif CONFIG == 'rasa_nlu_config.yml':
+        download_mitie_model_if_need()
+
     train(mdfile, nlu_config, base_dir=base, path=base / 'models')
 
 
