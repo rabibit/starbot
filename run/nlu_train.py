e0ff8dad1e4e841b45688523398272545cc4059c
Kevin Wang <wy721@qq.com>
2019-05-08 14:51:12 +0800

    refine

--- run/nlu_train.py
+++ run/nlu_train.py
@@ -16,19 +16,6 @@ BERT_MODEL_DIRNAME = "chinese_L-12_H-768_A-12"
 MITIE_MODEL_URL = "https://cloud.kvin.wang:8443/s/XEeQkZeqYb7fDYT/download"
 MITIE_MODEL_FILE = "total_word_feature_extractor.dat"
 
-if len(sys.argv) != 2:
-    print("Usage ./nlu_train.py bert|mitie")
-    sys.exit()
-
-# TODO: better way
-if sys.argv[1] == 'bert':
-    CONFIG = "bert_nlu_config.yml"
-elif sys.argv[1] == 'mitie':
-    CONFIG = "mitie_nlu_config.yml"
-else:
-    print("Usage ./nlu_train.py bert|mitie")
-    sys.exit()
-
 logger = logging.getLogger(__name__)
 base = Path(__file__).parent
 
@@ -54,6 +41,19 @@ def download_mitie_model_if_need():
             shutil.move(tmpfilename, base / MITIE_MODEL_FILE)
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage ./nlu_train.py bert|mitie")
+        sys.exit()
+
+    # TODO: better way
+    if sys.argv[1] == 'bert':
+        CONFIG = "bert_nlu_config.yml"
+    elif sys.argv[1] == 'mitie':
+        CONFIG = "mitie_nlu_config.yml"
+    else:
+        print("Usage ./nlu_train.py bert|mitie")
+        sys.exit()
+
     mdfile = base / 'data' / 'nlu.md'
     # TODO: 换个地方
     if CONFIG == 'bert_nlu_config.yml':
