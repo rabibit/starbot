bc9efb14c1905ad55a7802ce542066f3b22ababd
Kevin Wang <wy721@qq.com>
2019-07-02 17:02:59 +0800

    refine run.py

--- run/run.py
+++ run/run.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
 
 import logging
-import os
 import sys
+import os
 
-sys.path.append(os.path.abspath('nlu'))
-
-from rasa.core.broker import PikaProducer
-from rasa.core.interpreter import NaturalLanguageInterpreter
-from rasa.core.tracker_store import TrackerStore
-from rasa.core import utils as rsutils
-from rasa.core.run import serve_application
-from rasa.core.agent import Agent
-from rasa.core.processor import MessageProcessor
+from rasa.core.processor import MessageProcessor, UserMessage, DialogueStateTracker
 from rasa.core.events import BotUttered
 
 logger = logging.getLogger()  # get the root logger
@@ -22,38 +14,24 @@ def at_root(p):
     return os.path.abspath(p)
 
 
+def preprocess(message, tracker):
+    def is_question(message):
+        return '?' in message.text or '？' in message.text
+
+    message.origin_text = message.text
+    if tracker:
+        q = tracker.latest_bot_utterance
+        if isinstance(q, BotUttered) and q.text and is_question(q):
+            message.text = q.text + message.text
+    return message
+
+
 class StarMessageProcessor(MessageProcessor):
-    async def log_message(self, message):
-        # type: (UserMessage) -> Optional[DialogueStateTracker]
-
-        # we have a Tracker instance for each user
-        # which maintains conversation state
-        tracker = self._get_tracker(message.sender_id)
-
-        # preprocess message if necessary
-        if self.message_preprocessor is not None:
-            message = self.message_preprocessor(message, tracker)
-
-        if tracker:
-            await self._handle_message_with_tracker(message, tracker)
-            # save tracker state to continue conversation from this state
-            self._save_tracker(tracker)
-        else:
-            logger.warning("Failed to retrieve or create tracker for sender "
-                           "'{}'.".format(message.sender_id))
-        return tracker
-
-    @staticmethod
-    def preprocessor(message, tracker):
-        def is_question(message):
-            return '?' in message.text or '？' in message.text
-
-        message.origin_text = message.text
-        if tracker:
-            q = tracker.latest_bot_utterance
-            if isinstance(q, BotUttered) and q.text and is_question(q):
-                message.text = q.text + message.text
-        return message
+    async def _handle_message_with_tracker(
+        self, message: UserMessage, tracker: DialogueStateTracker
+    ):
+        message = preprocess(message, tracker)
+        return super(StarMessageProcessor, self)._handle_message_with_tracker(message, tracker)
 
     async def _parse_message(self, message):
         parsed_data = await super(StarMessageProcessor, self)._parse_message(message)
@@ -61,60 +39,18 @@ class StarMessageProcessor(MessageProcessor):
         return parsed_data
 
 
-class StarAgent(Agent):
-    def create_processor(self, preprocessor=None):
-        self._ensure_agent_is_ready()
-        mp = StarMessageProcessor(
-            self.interpreter,
-            self.policy_ensemble,
-            self.domain,
-            self.tracker_store,
-            self.nlg,
-            action_endpoint=self.action_endpoint,
-            message_preprocessor=preprocessor)
-        if preprocessor is None:
-            mp.message_preprocessor = mp.preprocessor
-        return mp
+def patch_it():
+    from rasa.core import processor
+    processor.MessageProcessor = StarMessageProcessor
 
-
-def load_agent(core_model, interpreter, endpoints,
-               tracker_store=None):
-    if endpoints.model:
-        raise NotImplementedError
-    else:
-        return StarAgent.load(core_model,
-                              interpreter=interpreter,
-                              generator=endpoints.nlg,
-                              tracker_store=tracker_store,
-                              action_endpoint=endpoints.action)
+    from rasa.core import agent
+    agent.MessageProcessor = StarMessageProcessor
 
 
 if __name__ == '__main__':
-    # Running as standalone python application
-    #rsutils.configure_colored_logging(logging.DEBUG)
+    patch_it()
 
-    logger.info("Rasa process starting")
+    from rasa.__main__ import main
+    sys.argv = sys.argv[:1] + ['run']
+    main()
 
-    core = at_root('rasa_prj/models')
-    nlu = at_root('rasa_prj/models')
-    endpoints = at_root('rasa_prj/endpoints.yml')
-    credentials = at_root('rasa_prj/credentials.yml')
-
-    port = 5002
-    connector = None
-
-    _endpoints = rsutils.AvailableEndpoints.read_endpoints(endpoints)
-
-    _interpreter = NaturalLanguageInterpreter.create(nlu,
-                                                     _endpoints.nlu)
-    _broker = PikaProducer.from_endpoint_config(_endpoints.event_broker)
-
-    _tracker_store = TrackerStore.find_tracker_store(
-        None, _endpoints.tracker_store, _broker)
-
-    _agent = load_agent(core,
-                        interpreter=_interpreter,
-                        tracker_store=_tracker_store,
-                        endpoints=_endpoints)
-
-    serve_application(_agent, connector, port, credentials)
