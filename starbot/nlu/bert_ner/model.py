ee532b6ef557d8a1470b2593c2d4e12eac598b50
Kevin Wang <wy721@qq.com>
2019-07-03 10:12:10 +0800

    concat c with 16 ones

--- starbot/nlu/bert_ner/model.py
+++ starbot/nlu/bert_ner/model.py
@@ -267,8 +267,8 @@ class IntentClassificationModel:
         if labels is not None:
             output_c = output_c[:16, :]
         output_c = tf.matmul(output_c, weight) + bias
-        output_c = tf.reduce_mean(output_c)
         output_c = tf.sigmoid(output_c)
+        output_c = tf.concat(output_c, tf.ones((16, 1)), 0)
 
         with tf.variable_scope("loss"):
             probabilities = tf.nn.softmax(output_p, axis=1)
