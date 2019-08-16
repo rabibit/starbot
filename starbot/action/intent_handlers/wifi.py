b79d95a371f88046b33352c5b506ea47aabe4147
Kevin Wang <wy721@qq.com>
2019-08-16 11:10:37 +0800

    Match wifi intent

--- starbot/action/intent_handlers/wifi.py
+++ starbot/action/intent_handlers/wifi.py
@@ -7,8 +7,7 @@ from rasa_sdk.executor import CollectingDispatcher, Tracker
 class WifiHandler(HandlerBase):
     def match(self, tracker: Tracker, domain: Dict[Text, Any]) -> bool:
         if self.is_last_message_user(tracker) and self.get_last_user_intent(tracker) in (
-            ''
-
+            'ask_for_wifi_info', 'ask_for_wifi_password'
         ):
             return True
         else:
