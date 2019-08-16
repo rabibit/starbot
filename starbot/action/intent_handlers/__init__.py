aee01d715111fe3405636418d5417ad1f811bd69
wangyouquan <wangyouquan@star-net.cn>
2019-08-16 18:00:44 +0800

    添加simple意图的action

--- starbot/action/intent_handlers/__init__.py
+++ starbot/action/intent_handlers/__init__.py
@@ -3,8 +3,10 @@ from typing import List
 from . import wifi
 from . import ask_if_ferry
 from . import bye
+from . import simple
 from .handler import BaseHandler
 
 handlers: List[BaseHandler] = [wifi.WifiHandler(),
                                bye.ByeHandler(),
-                               ask_if_ferry.AskIfFerryHandler()]
+                               ask_if_ferry.AskIfFerryHandler(),
+                               simple.SimpleHandler()]
