792ea4d22707cb1547324e1ef043ca9d109f343b
wangyouquan <wangyouquan@star-net.cn>
2019-08-16 14:36:48 +0800

    添加意图相应的action

---
+++ starbot/action/intent_handlers/bye.py
@@ -0,0 +1,22 @@
+
+from .handler import BaseHandler
+from typing import Text, Dict, Any, List
+from rasa_sdk.executor import CollectingDispatcher, Tracker
+
+
+class WifiHandler(BaseHandler):
+    def match(self, tracker: Tracker, domain: Dict[Text, Any]) -> bool:
+        if self.is_last_message_user(tracker) and self.get_last_user_intent(tracker) in (
+            'ask_for_wifi_info', 'ask_for_wifi_password'
+        ):
+            return True
+        else:
+            return False
+
+    def process(self,
+                dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_template("utter_wifi_info", tracker)
+        return []
+
