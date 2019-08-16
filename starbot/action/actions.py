c73b202970e7b5028748b6dd1ac989cafba183d1
Kevin Wang <wy721@qq.com>
2019-08-16 11:16:08 +0800

    Add log

--- starbot/action/actions.py
+++ starbot/action/actions.py
@@ -1,8 +1,11 @@
+import logging
 from rasa_sdk import Action
 
 from typing import Text, Dict, Any
 from rasa_sdk.executor import CollectingDispatcher, Tracker
 from .intent_handlers import handlers
+
+logger = logging.getLogger(__name__)
 
 
 class ProcessIntentAction(Action):
@@ -12,6 +15,7 @@ class ProcessIntentAction(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]):
         for handler in handlers:
             if handler.match(tracker, domain):
+                logger.debug(f'Matched handler {handler} for {tracker.latest_message}')
                 if handler.process(dispatcher, tracker, domain):
                     break
 
