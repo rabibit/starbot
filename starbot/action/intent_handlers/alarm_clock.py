33d1ed769bf05c51e1afaac3d37833975b611a02
wangyouquan <wangyouquan@star-net.cn>
2019-08-19 10:15:48 +0800

    细化闹钟设置任务的action

---
+++ starbot/action/intent_handlers/alarm_clock.py
@@ -0,0 +1,39 @@
+
+from .handler import BaseHandler
+from typing import Text, Dict, Any, List, Optional
+from rasa_sdk.executor import CollectingDispatcher, Tracker
+from rasa_sdk.events import Form
+
+
+class AlarmClockHandler(BaseHandler):
+    def match(self, tracker: Tracker, domain: Dict[Text, Any]) -> bool:
+        return True
+
+    def process(self,
+                dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: Dict[Text, Any]) -> Optional[List[Dict[Text, Any]]]:
+        intent = self.get_last_user_intent(tracker)
+        if intent in {
+            'ask_for_awaking'
+        }:
+            # 问闹钟设置时间
+            alarm_time = self.get_entity(tracker, 'time')
+            if alarm_time is None:
+                dispatcher.utter_message("请问您要设置几点的闹钟?")
+                return [Form('alarm_clock')]
+            else:
+                dispatcher.utter_message('好的，闹钟已设置，时间为{}'.format(alarm_time))
+                return [Form(None)]
+        else:
+            if tracker.active_form.get('name') == 'alarm_clock':
+                alarm_time = self.get_entity(tracker, 'time')
+                if alarm_time is not None:
+                    # 找到alarm_time后完结表单
+                    dispatcher.utter_message('好的，闹钟已设置，时间为{}'.format(alarm_time))
+                    return [Form(None)]
+                else:
+                    dispatcher.utter_message("请问您要设置几点的闹钟?")
+                    return []
+        return None
+
