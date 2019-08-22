0475cb5c3eba17894066e7f9fb915a173bfbd3cf
Kevin Wang <wy721@qq.com>
2019-08-22 08:59:59 +0800

    Refactor: rename files

--- starbot/__init__.py
+++ starbot/__init__.py
@@ -1,5 +1,5 @@
 from starbot.nlu.tokenizers import CharTokenizer, ThulacTokenizer, ThulacNimTokenizer
 from starbot.nlu.bert_ner.ner import BertExtractor
 from starbot.nlu.bert_ner.litener import LiteExtractor
-from starbot.nlu.classifiers.charfreqclassifier import CharFreqClassifier
-from starbot.nlu.clear_embedding import ClearEmbedding
+from starbot.nlu.pipelines.charfreqclassifier import CharFreqClassifier
+from starbot.nlu.pipelines.clear_embedding import ClearEmbedding
