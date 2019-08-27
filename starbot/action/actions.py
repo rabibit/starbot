429ab55f0d63cf5eff22de4d5e9187c09dcc65fd
wangyouquan <wangyouquan@star-net.cn>
2019-08-27 11:21:40 +0800

    用MyDispatcher类来实现多个句子合并成一个句子

--- starbot/action/actions.py
+++ starbot/action/actions.py
@@ -12,20 +12,13 @@ import random
 logger = logging.getLogger(__name__)
 
 
-class MyDispatcher(CollectingDispatcher):
-    def utter_message(self, text, **kwargs):
-        # type: (Text, Any) -> None
-        """"Send a text to the output channel"""
+class MyDispatcher():
+    def __init__(self):
+        self.messages: [str] = []
 
-        tmp_messages = self.messages.copy()
-        for var in tmp_messages:
-            if len(var) == 1 and var.keys()[0] == "text":
-                text += var.values()[0]
-                self.messages.remove(var)
-        message = {"text": text}
-        message.update(kwargs)
+    def utter_message(self, text: str):
 
-        self.messages.append(message)
+        self.messages.append(text)
 
 
 class ProcessIntentAction(Action):
@@ -33,10 +26,11 @@ class ProcessIntentAction(Action):
         return "action_process_intent"
 
     def run(self,
-            dispatcher: MyDispatcher,
+            dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         what_msg = random.choice(['啥', '你说啥', '什么']) + random.choice(['我没听清', ''])
+        my_dispatcher = MyDispatcher()
         if tracker.latest_message:
             if is_last_message_user(tracker):
                 msg = '\n'.join([f'{key}: {val}' for key, val in tracker.latest_message.items()])
@@ -51,13 +45,15 @@ class ProcessIntentAction(Action):
             handler = intent_to_handlers[tracker.latest_message.get('intent', {}).get('name')]()
             if handler.continue_form() is False:
                 tracker.slots = {}
-            events = handler.process(dispatcher, tracker, domain)
+            events = handler.process(my_dispatcher, tracker, domain)
             logger.debug(f'Handler {handler} processed')
             if tracker.active_form.get('name') in form_to_handlers.keys() and handler.continue_form():
                 handler = form_to_handlers[tracker.active_form.get('name')]()
-                events += handler.process(dispatcher, tracker, domain)
+                events += handler.process(my_dispatcher, tracker, domain)
             if events is None:
+                dispatcher.utter_message("".join(my_dispatcher.messages))
                 return []
+            dispatcher.utter_message("".join(my_dispatcher.messages))
             return events
         else:
             for Handler in intent_to_handlers.values():
@@ -68,6 +64,7 @@ class ProcessIntentAction(Action):
                 if events is None:
                     continue
                 logger.debug(f'Handler {handler} processed')
+                dispatcher.utter_message("".join(my_dispatcher.messages))
                 return events
         dispatcher.utter_message(what_msg)
         return []
