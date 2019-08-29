1804a652b9fa2a0baddc90a8a3307c21178cb589
Kevin Wang <wy721@qq.com>
2019-08-29 10:39:22 +0800

    解析闹钟时间

--- starbot/action/intent_handlers/alarm_clock.py
+++ starbot/action/intent_handlers/alarm_clock.py
@@ -4,6 +4,8 @@ from typing import Text, Dict, Any, List, Optional
 from rasa_sdk.executor import CollectingDispatcher, Tracker
 from rasa_sdk.events import Form
 
+from starbot.nlu.timeparser.timeparser import extract_times
+
 
 class AlarmClockHandler(BaseHandler):
     def match(self, tracker: Tracker, domain: Dict[Text, Any]) -> bool:
@@ -19,21 +21,33 @@ class AlarmClockHandler(BaseHandler):
         }:
             # 问闹钟设置时间
             alarm_time = self.get_entity(tracker, 'time')
-            if alarm_time is None:
-                dispatcher.utter_message("请问您要设置几点的闹钟?")
+            times = list(extract_times(alarm_time or ''))
+
+            if alarm_time:
+                dispatcher.utter_message('/parsed {} -> {}'.format(alarm_time, times))
+
+            if not times:
+                dispatcher.utter_message("好的，啥时候提醒您?")
+                return [Form('alarm_clock')]
+            elif times[-1].hour is None:
+                dispatcher.utter_message(f"好的，{alarm_time}几点钟提醒您?")
                 return [Form('alarm_clock')]
             else:
-                dispatcher.utter_message('好的，闹钟已设置，时间为{}'.format(alarm_time))
+                time = times[0]
+                dispatcher.utter_message('好的，{}我会电话给你'.format(alarm_time))
                 return [Form(None)]
         else:
             if tracker.active_form.get('name') == 'alarm_clock':
                 alarm_time = self.get_entity(tracker, 'time')
-                if alarm_time is not None:
+                times = list(extract_times(alarm_time or ''))
+                if alarm_time:
+                    dispatcher.utter_message('/parsed {} -> {}'.format(alarm_time, times))
+                if times and times[-1].hour is not None:
                     # 找到alarm_time后完结表单
-                    dispatcher.utter_message('好的，闹钟已设置，时间为{}'.format(alarm_time))
+                    dispatcher.utter_message('{}好的，到时候我会电话给你'.format(alarm_time))
                     return [Form(None)]
                 else:
-                    dispatcher.utter_message("请问您要设置几点的闹钟?")
+                    dispatcher.utter_message("啥时候提醒你?")
                     return []
         return None
 
