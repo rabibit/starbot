37b067d2fa4f69490d52eaa4e2e0dc9b80bec2d6
Kevin Wang <wy721@qq.com>
2019-09-03 10:55:36 +0800

    Update charger handler to use "brand" entity

--- starbot/action/intent_handlers/charger.py
+++ starbot/action/intent_handlers/charger.py
@@ -7,16 +7,21 @@ class ChargerHandler(BaseFormHandler):
     class Form(BaseForm):
         __tag__ = 'charger'
         charger_type: Text
+        brand: Text
     form: Form
 
     def form_trigger(self, intent: Text):
         return intent == 'ask_for_charger'
 
+    @property
+    def charger_type(self):
+        return self.form.charger_type or self.form.brand
+
     def validate(self):
-        if self.form.charger_type is None:
+        if self.charger_type is None:
             self.utter_message('什么充电器?')
             return False
         return True
 
     def commit(self):
-        self.utter_message('好的, 服务员一会儿给你送过去')
+        self.utter_message(f'{self.charger_type}的充电器, 是吧, 服务员一会儿给你送过去')
