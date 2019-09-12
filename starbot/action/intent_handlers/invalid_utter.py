d9a6e2fb79c70f93c50756b89b43d7da8c0299a1
wangyouquan <wangyouquan@star-net.cn>
2019-09-12 13:50:30 +0800

    index on invalid: bf3164c Refactor the skiping policies

---
+++ starbot/action/intent_handlers/invalid_utter.py
@@ -0,0 +1,30 @@
+import re
+
+from .handler import BaseHandler
+from typing import Text, Dict, Any, List, Optional
+
+
+class QuitHandler(BaseHandler):
+    not_pat = re.compile(r'[没谁]说?[叫让]你|我不要|我没有|我没说|神经病')
+
+    def match(self) -> bool:
+        return True
+
+    def process(self) -> Optional[List[Dict[Text, Any]]]:
+        if not self.tracker.active_form:
+            return None
+        if not self.tracker.latest_message:
+            return None
+
+        commands = set(self.tracker.latest_message.get('commands') or [])
+
+        events = None
+        if {'exit', 'cancel'} & commands:
+            events = self.context.cancel_form(force=True)
+        else:
+            message = self.tracker.latest_message['text']
+            if self.not_pat.search(message):
+                events = self.context.cancel_form(force=False)
+        if events is not None:
+            self.utter_message('好的， 还有其它需要吗')
+        return events
