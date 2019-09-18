8fc30b0cfbb72bd03883d9945e9b9f747939940a
Kevin Wang <wy721@qq.com>
2019-09-18 13:52:27 +0800

    查号码使用FormHandler

--- starbot/action/intent_handlers/ask_for_phone_number.py
+++ starbot/action/intent_handlers/ask_for_phone_number.py
@@ -1,32 +1,21 @@
 
-from .handler import BaseHandler
-from typing import Text, Dict, Any, List, Optional
-from rasa_sdk.events import Form
+from .handler import BaseFormHandler, BaseForm
+from typing import Text
 
 
-class AskForPhoneNumberHandler(BaseHandler):
-    def match(self) -> bool:
+class AskForPhoneNumberHandler(BaseFormHandler):
+    class Form(BaseForm):
+        __tag__ = 'subject_of_phone_number'
+        subject_of_phone_number: str
+
+    def form_trigger(self, intent: Text):
+        return intent == 'ask_for_phone_number'
+
+    def validate(self, recovering: bool):
+        if self.form.subject_of_phone_number is None:
+            self.utter_message("请问您要查询什么电话号码?")
+            return False
         return True
 
-    def process(self):
-        intent = self.get_last_user_intent()
-        if intent in {
-            'ask_for_phone_number'
-        }:
-            subject = self.get_entity('subject_of_phone_number')
-            if subject is None:
-                self.utter_message("请问您要查询什么电话号码?")
-                self.activate_form('subject_of_phone_number')
-            else:
-                self.utter_message('{}的电话号码是123456789'.format(subject))
-                self.activate_form(None)
-        elif self.tracker.active_form.get('name') == 'subject_of_phone_number':
-            subject = self.get_entity('subject_of_phone_number')
-            if subject is not None:
-                # 找到subject_of_phone_number后完结表单
-                self.utter_message('{}的电话号码是123456789'.format(subject))
-                self.activate_form(None)
-            else:
-                self.utter_message("请问您要查询什么电话号码?")
-        else:
-            self.skip()
+    def commit(self):
+        self.utter_message('{}的电话号码是123456789'.format(self.form.subject_of_phone_number))
